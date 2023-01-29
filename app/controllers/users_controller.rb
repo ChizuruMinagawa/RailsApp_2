b5d8d2c1eb61e956854b9689a2114aa419832796
皆川千鶴 <minagawachizuru@minakawachidzurunoMacBook-Pro.local>
2023-01-29 23:13:36 +0900

    最低限の見た目のusersページ作成(top,sign_up/in,account,profile)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def account
+  end
+
+  def profile
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
